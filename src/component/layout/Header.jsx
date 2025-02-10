f7dc87e903a0e79409efee20bb09f87ec2249426
kimjinwoo <upbreak05@naver.com>
2025-02-10 11:41:16 +0900

    공지사항 슬라이더 컴포넌트
    공지사항 목록을 순차적으로 보여주는 컴포넌트 작성
    아직 공지사항 리스트를 가져오는 api가 없어서 임시데이터로 보여주고 있음
    추후에 수정 필요

--- src/component/layout/Header.jsx
+++ src/component/layout/Header.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+import AnnouncementSlider from "../module/AnnouncementSlider";
 
 const Header = () => {
     const [isSidebarToggled, setIsSidebarToggled] = useState(false);
@@ -24,18 +25,15 @@ const Header = () => {
     return(
         <nav className="sb-topnav navbar navbar-expand navbar-dark bg-dark">
             {/* Navbar Brand*/}
-            <a className="navbar-brand ps-3" href="index.html">현장근태관리시스템</a>
+            <a className="navbar-brand ps-3" href="index.html">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;공사관리시스템</a>
             {/* Sidebar Toggle*/}
             <button className="btn btn-link btn-sm order-1 order-lg-0 me-4 me-lg-0" id="sidebarToggle" onClick={handleSidebarToggle}><i className="fas fa-bars" /></button>
             {/* Navbar Search*/}
-            <form className="d-none d-md-inline-block form-inline ms-auto me-0 me-md-3 my-2 my-md-0">
-                <div className="input-group">
-                <input className="form-control" type="text" placeholder="Search for..." aria-label="Search for..." aria-describedby="btnNavbarSearch" />
-                <button className="btn btn-primary" id="btnNavbarSearch" type="button"><i className="fas fa-search" /></button>
-                </div>
+            <form className="d-none d-md-inline-block form-inline">
+                <AnnouncementSlider />
             </form>
             {/* Navbar*/}
-            <ul className="navbar-nav ms-auto ms-md-0 me-3 me-lg-4">
+            <ul className="navbar-nav ms-auto me-0 me-md-3 my-2 my-md-0">
                 <li className="nav-item dropdown">
                 <a className="nav-link dropdown-toggle" id="navbarDropdown" href="#!" role="button" data-bs-toggle="dropdown" aria-expanded="false"><i className="fas fa-user fa-fw" /></a>
                 <ul className="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
---
+++ src/component/module/AnnouncementSlider.jsx
@@ -0,0 +1,102 @@
+import React, { useState, useEffect } from "react";
+import "../../assets/css/slider.css";
+import arrorRight from "../../assets/image/arrow-right.png";
+import arrorLeft from "../../assets/image/arrow-left.png";
+
+const announcements = [
+  "공지 1: 시스템 점검 예정",
+  "공지 2: 신규 기능 추가",
+  "공지 3: 이벤트 참여 안내",
+  "공지 4: 업데이트 소식1111111111111111111111111111111111111111111111111",
+];
+
+function AnnouncementSlider() {
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [isAnimating, setIsAnimating] = useState(false);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      handleNext();
+    }, 3000);
+    return () => clearInterval(interval);
+  }, [currentIndex]);
+
+  const resetAutoSlide = () => {
+    clearInterval(window.autoSlide);
+    window.autoSlide = setInterval(handleNext, 3000);
+  };
+
+  const handlePrev = () => {
+    if (isAnimating) return;
+    setIsAnimating(true);
+    setCurrentIndex((prevIndex) =>
+      prevIndex === 0 ? announcements.length - 1 : prevIndex - 1
+    );
+    resetAutoSlide();
+  };
+
+  const handleNext = () => {
+    if (isAnimating) return;
+    setIsAnimating(true);
+    setCurrentIndex((prevIndex) => (prevIndex + 1) % announcements.length);
+    resetAutoSlide();
+  };
+
+  const handleAnnouncementClick = () => {
+    alert(`현재 공지사항: ${announcements[currentIndex]}`);
+    console.log(`공지사항 클릭: ${announcements[currentIndex]}`);
+  };
+
+  useEffect(() => {
+    const timeout = setTimeout(() => setIsAnimating(false), 500);
+    return () => clearTimeout(timeout);
+  }, [currentIndex]);
+
+  return (
+    <div className="announcement-slider">
+      {/* 왼쪽 화살표 버튼 */}
+      <img
+            src={arrorLeft}
+            onClick={handlePrev}
+            style={{ 
+            width: "12px", 
+            zIndex: 10, 
+            cursor: "pointer",
+            filter: "brightness(0) invert(1)",
+            marginRight: "10px"
+            }}
+            alt="left arrow"
+        />
+
+      {/* 슬라이드 텍스트 */}
+      <div className="slides-container">
+        <div className="slide">
+          <span className="slide"
+            onClick={handleAnnouncementClick}
+            style={{
+              cursor: "pointer",
+            }}
+          >
+            {announcements[currentIndex]}
+          </span>
+        </div>
+      </div>
+
+      {/* 오른쪽 화살표 버튼 */}
+      <img
+            src={arrorRight}
+            onClick={handleNext}
+            style={{ 
+                width: "12px", 
+                zIndex: 10, 
+                cursor: "pointer",
+                filter: "brightness(0) invert(1)",
+                marginLeft: "10px"
+            }}
+            alt="right arrow"
+        />
+    </div>
+  );
+}
+
+export default AnnouncementSlider;
