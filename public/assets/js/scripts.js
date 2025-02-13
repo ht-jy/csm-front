de821c1b3315fc83cd59501f0bcc6e6c9026fd50
kimjinwoo <upbreak05@naver.com>
2025-02-13 12:02:54 +0900

    Revert "근태인식기 관리 화면 작성"
    This reverts commit 1422891e35313752ede643709a64a40902d60cc5.

--- public/assets/js/scripts.js
+++ public/assets/js/scripts.js
@@ -8,14 +8,17 @@
 // 
 
 window.addEventListener('DOMContentLoaded', event => {
+    console.log('Scripts.js window.addEventListener')
     // Toggle the side navigation
     const sidebarToggle = document.body.querySelector('#sidebarToggle');
+    console.log(sidebarToggle);
     if (sidebarToggle) {
         // Uncomment Below to persist sidebar toggle between refreshes
         // if (localStorage.getItem('sb|sidebar-toggle') === 'true') {
         //     document.body.classList.toggle('sb-sidenav-toggled');
         // }
         sidebarToggle.addEventListener('click', event => {
+            console.log('sidebarToggle click');
             event.preventDefault();
             document.body.classList.toggle('sb-sidenav-toggled');
             localStorage.setItem('sb|sidebar-toggle', document.body.classList.contains('sb-sidenav-toggled'));
