d203f8c64ab18c11da8e3438d6dd4427e0735ff5
htjy <jyjeong2412@htenc.co.kr>
2025-03-28 10:40:17 +0900

    [지도] 지도 zoom 기능 추가
    1. zoomIn, zoomOut 기능 추가

--- src/component/module/Map.jsx
+++ src/component/module/Map.jsx
@@ -2,6 +2,9 @@ import { useEffect, useState } from "react";
 import {Axios} from "../../utils/axios/Axios"
 import markerIcon from "../../assets/image/marker.png"
 import Modal from "./Modal";
+import zoomInIcon from "../../assets/image/zoom-in.png"
+import zoomOutIcon from "../../assets/image/zoom-out.png"
+import ExitIcon from "../../assets/image/exit.png"
 /**
  * @description: vworld의 지도 보여주는 컴포넌트
  * 
@@ -24,6 +27,7 @@ const Map = ( {roadAddress} ) => {
     const [point, setPoint] = useState(null);
     const [map, setMap] = useState(null);
     const [isModal, setIsModal] = useState(false);
+    const [zoomOpen, setZoomOpen] = useState(false);
 
     const getPoint = async () => {
         // vworld상 좌표값을 얻기 위한 요청
@@ -34,6 +38,46 @@ const Map = ( {roadAddress} ) => {
             setIsModal(true);
         }
     }
+
+   const handleZoomClick = () => {
+        if (point === null) return;
+        console.log("zoomOpen", zoomOpen)
+        window.vw.ol3.MapOptions = {
+            basemapType: window.vw.ol3.BasemapType.GRAPHIC
+            , controlDensity: window.vw.ol3.DensityType.EMPTY
+            , interactionDensity: window.vw.ol3.DensityType.BASIC
+            , controlsAutoArrange: true
+            , homePosition: window.vw.ol3.CameraPosition
+            , initPosition: window.vw.ol3.CameraPosition
+        } 
+
+        const vMapZoom = new window.vw.ol3.Map("vMapZoom", window.vw.ol3.MapOptions);
+        console.log(vMapZoom.getView())
+        vMapZoom.getView().setCenter([Number(point.x), Number(point.y)]);
+        vMapZoom.getView().setZoom(18);
+
+            // 마커 설정하기
+        const markerLayer = new window.vw.ol3.layer.Marker(vMapZoom);
+        vMapZoom.addLayer(markerLayer);
+        window.vw.ol3.markerOption = {
+            x : point.x,
+            y : point.y,
+            epsg : "EPSG:900913",
+            iconUrl : markerIcon,
+            
+            text : {
+                offsetX: 0.5, //위치설정
+                offsetY: 20,   //위치설정
+                fill: {color: '#000'},
+                stroke: {color: '#fff', width: 2},
+            },
+        }
+
+        markerLayer.addMarker(window.vw.ol3.markerOption);
+            
+    }
+
+
     useEffect( () => {
 
         if (map && point) {
@@ -65,7 +109,45 @@ const Map = ( {roadAddress} ) => {
 
     }, [point])
     
-  
+    useEffect(() => {
+
+        if (point === null) return;
+        if(zoomOpen === false) return;
+
+        window.vw.ol3.MapOptions = {
+            basemapType: window.vw.ol3.BasemapType.GRAPHIC
+            , controlDensity: window.vw.ol3.DensityType.EMPTY
+            , interactionDensity: window.vw.ol3.DensityType.BASIC
+            , controlsAutoArrange: true
+            , homePosition: window.vw.ol3.CameraPosition
+            , initPosition: window.vw.ol3.CameraPosition
+        } 
+
+        const vMapZoom = new window.vw.ol3.Map("vMapZoom", window.vw.ol3.MapOptions);
+        console.log(vMapZoom.getView())
+        vMapZoom.getView().setCenter([Number(point.x), Number(point.y)]);
+        vMapZoom.getView().setZoom(18);
+
+            // 마커 설정하기
+        const markerLayer = new window.vw.ol3.layer.Marker(vMapZoom);
+        vMapZoom.addLayer(markerLayer);
+        window.vw.ol3.markerOption = {
+            x : point.x,
+            y : point.y,
+            epsg : "EPSG:900913",
+            iconUrl : markerIcon,
+            
+            text : {
+                offsetX: 0.5, //위치설정
+                offsetY: 20,   //위치설정
+                fill: {color: '#000'},
+                stroke: {color: '#fff', width: 2},
+            },
+        }
+
+        markerLayer.addMarker(window.vw.ol3.markerOption);
+
+    }, [zoomOpen])
     useEffect(() => {
         if (map === null) {
 
@@ -96,8 +178,91 @@ const Map = ( {roadAddress} ) => {
                 confirm={"확인"}
                 fncConfirm={() => setIsModal(false)}
             />
-            <div id="vMap"  style={{width:"100%", height:"100%", left:"0px", top:"0px"}}/>        
+
+            { zoomOpen &&
+                <div style={{...overlayStyle}} >
+                    <div style={{...modalStyle}} >
+                        <img
+                            style={{
+                                position: "absolute",
+                                top: "10vh",
+                                right: "12vw",
+                                zIndex: 1000,
+                                width:"30px",
+                                backgroundColor:"rgba(0, 0, 0, 0.2)",
+                                borderRadius: "5px"
+            
+                            }}
+                            src={zoomOutIcon}
+                            onClick={() => setZoomOpen(false)}
+                            ></img>
+                            <div id="vMapZoom"  style={mapStyle}></div>
+                    </div>
+                </div>
+            }
+
+            <div style={{ position: "relative", width: "100%", height: "100%" }}>
+                <div id="vMap"  style={{width:"100%", height:"100%", left:"0px", top:"0px"}}></div>
+                <img
+                    style={{
+                    position: "absolute",
+                    top: "5px",
+                    right: "5px",
+                    zIndex: 1000,
+                    width:"30px",
+                    backgroundColor:"rgba(0, 0, 0, 0.2)",
+                    borderRadius: "5px"
+                    }}
+
+                    src={zoomInIcon}
+                    onClick={() => {
+                        setZoomOpen(true);
+                        handleZoomClick()
+                    }}
+                >
+                </img>
+            </div>       
+
         </>
     );
 }
+
+
+const overlayStyle = {
+    position: 'fixed',
+    top: 0,
+    left: 0,
+    width: '100%',
+    height: '100%',
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+    display: 'flex',
+    justifyContent: 'center',
+    zIndex: 9998,
+}
+
+const modalStyle = {
+    display:"flex",
+    flexDirection:"column", 
+    // padding : '5px',
+    margin : '8vh 0px',
+    backgroundColor: '#ececec',
+    borderRadius: '8px',
+    border: "1px solid rgba(0, 0, 0, 1)",
+    maxWidth: '80vw',
+    width: '80vw',
+    maxHeight: '80vh',
+    height: '80vh',
+    boxShadow: '0 4px 6px rgba(0, 0, 0, 0.1)',
+    zIndex: 100,
+};
+
+const mapStyle = {
+    // border : "1px solid rgba(0, 0, 0, 0.3)",
+    boxShadow: '0 4px 6px rgba(0, 0, 0, 0.1)',
+    width:"100%", 
+    height:"100%", 
+    left:"0px", 
+    top:"0px"
+}
+
 export default Map;
