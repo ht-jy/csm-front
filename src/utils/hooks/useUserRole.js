4c85fa08554876d76717dbbf81f030ae14008124
jwkim2412 <jwkim2412@htenc.co.kr>
2025-08-07 10:41:11 +0900

    협력업체 로그인시 현장근로자에서 동일 협력업체 근로자만 조회되도록 수정

--- src/utils/hooks/useUserRole.js
+++ src/utils/hooks/useUserRole.js
@@ -12,39 +12,6 @@ import { ObjChk } from '../ObjChk';
  *
  */
 
-// 기능별 권한 관리
-// 홈페이지에서 기능별 권한 설정을 하지 않아서 DB로 관리를 안하고 있음
-export const roleGroup = Object.freeze({
-    GRANT_TEMP_SITE_MANAGER: [ // 임시 현장관리자 부여 권한
-        userRole.SYSTEM_ADMIN.code,
-        userRole.SUPER_ADMIN.code,
-        userRole.ADMIN.code,
-        userRole.SITE_MANAGER.code,
-    ],
-    SITE_MANAGER: [ // 현장 등록 권한
-        userRole.SYSTEM_ADMIN.code,
-        userRole.SUPER_ADMIN.code,
-        userRole.ADMIN.code,
-        userRole.SITE_DIRECTOR.code,
-        userRole.SITE_MANAGER.code
-    ],
-    NOTICE_ADD_MANAGER: [ // 공지사항 등록 권한
-        userRole.SYSTEM_ADMIN.code,
-        userRole.SUPER_ADMIN.code,
-        userRole.ADMIN.code,
-        userRole.SITE_DIRECTOR.code,
-        userRole.SITE_MANAGER.code,
-        userRole.TEMP_SITE_MANAGER.code,
-        userRole.SAFETY_MANAGER.code,
-        userRole.SUPERVISOR.code,
-    ],
-    NOTICE_ALL_MOD_MANAGER: [ // 다른 사람이 작성한 공지사항 수정, 삭제 권한
-        userRole.SYSTEM_ADMIN.code,
-        userRole.SUPER_ADMIN.code,
-    ]
-
-});
-
 // 권한 체크
 export const useUserRole = () => {
     const {user, jobRole, userRole, project} = useAuth();
