a536ba9278e92e8e210ccc4b2675ca6e577400dd
정지영(99) <jyjeong2412@htenc.co.kr>
2025-09-08 12:01:01 +0900

    [권한] 협력업체 임시현장관리자, 현장관리자 분리

--- src/utils/rolesObject/companyRoles.js
+++ src/utils/rolesObject/companyRoles.js
@@ -2,9 +2,9 @@ import { userRole } from "../Enum";
 
 // 협력업체 기능별 권한
 export const CompanyRoles = Object.freeze({
-    GRANT_TEMP_SITE_MANAGER: [ // 임시 현장관리자 부여 권한
-        userRole.SYSTEM_ADMIN,
+    GRANT_TEMP_SITE_MANAGER: [ // 임시 현장관리자 부여/해제 권한
         userRole.SUPER_ADMIN,
+        userRole.SYSTEM_ADMIN,
         userRole.ADMIN,
         userRole.SITE_MANAGER,
     ],
